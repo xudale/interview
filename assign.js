b56d0ebf8a5953b42e26bb8b3c80f9f7fb71697d
xupengyue <xupengyuehalin@163.com>
2022-01-29 20:10:51 +0800

    2

--- assign.js
+++
@@ -1,9 +0,0 @@
-Object.assign = function(target, ...args) {
-	for (let i = 0; i < args.length; i++) {
-		const cur = args[i]
-		Object.keys(cur).forEach(key => {
-			target[key] = cur[key]
-		})
-	}
-	return target
-}
